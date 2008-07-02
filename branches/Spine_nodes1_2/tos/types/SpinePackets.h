be82bd0e46278e29910bbf9e8a8ac5edd3f23ddf
gravina <gravina@b32d262b-c4f0-137c-5a66-55235e244b8c>
2008-07-02 04:39:06 +0000

    re-organized some enums + added some new ones

--- branches/Spine_nodes1_2/tos/types/SpinePackets.h
+++ branches/Spine_nodes1_2/tos/types/SpinePackets.h
@@ -37,9 +37,6 @@ Boston, MA  02111-1307, USA.
 #include "message.h"
 
 enum PacketTypes {
-
-  AM_SPINE = 0x99,
-
   SERVICE_ADV = 0x02,
   DATA = 0x04,
   SVC_MSG = 0x06,               // to notify the coordinator of events, errors, warnings and other internal information.
@@ -48,8 +45,8 @@ enum PacketTypes {
   SETUP_SENSOR = 0x03,
   SETUP_FUNCTION = 0x05,
   START = 0x09,
-  RESET = 0x0B,                 // it is used as a BEACON (re-sync) message
-  STOP = 0x0D,
+  RESET = 0x0B,                 // simulate an hardware reset
+  SYNCR = 0x0D,                  // it is used as a BEACON (re-sync) message
   FUNCTION_REQ = 0x07           // contains a flag to specify if enable or disable the function
 };
 
@@ -57,16 +54,21 @@ enum {
   SPINE_HEADER_PKT_SIZE = 9,
   SPINE_PKT_PAYLOAD_MAX_SIZE = TOSH_DATA_LENGTH - SPINE_HEADER_PKT_SIZE,   // default will be 28 - 9 = 19 bytes
   SPINE_PKT_MAX_SIZE = SPINE_HEADER_PKT_SIZE + SPINE_PKT_PAYLOAD_MAX_SIZE,  // default will be 19 + 9 = 28 bytes
-  
+
   SPINE_SVC_MSG_SIZE = 2,                   // the SPINE header szie is not included
   SPINE_SVC_ADV_PKT_MAX_SIZE = 50,          // the SPINE header size is not included
   SPINE_SETUP_SENSOR_PKT_SIZE = 3,          // the SPINE header size is not included
-  SPINE_START_PKT_SIZE = 2                  // the SPINE header size is not included
+  SPINE_START_PKT_SIZE = 2,                 // the SPINE header size is not included
+  SPINE_FUNCTION_REQ_PKT_MAX_SIZE = 20,     // the SPINE header size is not included
+  SPINE_SETUP_FUNCTION_PKT_MAX_SIZE = 20,   // the SPINE header size is not included
+  SPINE_DATA_PKT_MAX_SIZE = 246             // the SPINE header size is not included  - note: max tinyos message payload is 255
 };
 
 enum {
-  SPINE_BASE_STATION = 0x0000,
-  SPINE_BROADCAST = 0xFFFF
+  AM_SPINE = 0x99,              // every SPINE packets will have the same AM Type
+
+  SPINE_BASE_STATION = 0x0000,  // reserved address: remote SPINE nodes can't be assigned with this address
+  SPINE_BROADCAST = 0xFFFF      // reserved address: any SPINE node can't be assigned with this address
 };
 
 #endif
