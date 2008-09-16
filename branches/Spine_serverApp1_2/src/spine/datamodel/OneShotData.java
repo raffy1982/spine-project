85879cc6fe7a1cadb6a5886a25cee6fbeee7dd94
giannantonio <giannantonio@b32d262b-c4f0-137c-5a66-55235e244b8c>
2008-09-16 08:29:56 +0000

    Comments clean-up

--- branches/Spine_serverApp1_2/src/spine/datamodel/OneShotData.java
+++ branches/Spine_serverApp1_2/src/spine/datamodel/OneShotData.java
@@ -1,26 +1,26 @@
 /*****************************************************************
 SPINE - Signal Processing In-Node Environment is a framework that 
-allows dynamic configuration of feature extraction capabilities 
-of WSN nodes via an OtA protocol
+allows dynamic on node configuration for feature extraction and a 
+OtA protocol for the management for WSN
 
 Copyright (C) 2007 Telecom Italia S.p.A. 
-Â 
+ 
 GNU Lesser General Public License
-Â 
+ 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation, 
 version 2.1 of the License. 
-Â 
+ 
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.Â  See the GNU
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 Lesser General Public License for more details.
-Â 
+ 
 You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the
 Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-Boston, MAÂ  02111-1307, USA.
+Boston, MA  02111-1307, USA.
 *****************************************************************/
 
 /**
