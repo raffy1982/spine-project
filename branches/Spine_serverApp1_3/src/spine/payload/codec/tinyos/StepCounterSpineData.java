f32dda7db87cd6088a1359c192745d8afea9be37
gravina <gravina@b32d262b-c4f0-137c-5a66-55235e244b8c>
2009-05-13 00:07:09 +0000

    bug fix in 'stepCount' field conversion.

--- branches/Spine_serverApp1_3/src/spine/payload/codec/tinyos/StepCounterSpineData.java
+++ branches/Spine_serverApp1_3/src/spine/payload/codec/tinyos/StepCounterSpineData.java
@@ -53,8 +53,17 @@ public class StepCounterSpineData extends SpineCodec {
 		
 		// set data.nodeID, data.functionCode e data.timestamp
 		data.baseInit(nodeID, payload);
-		data.setStepsCount((payload[2]<<8) | payload[3]);
+		data.setStepsCount(byteArrayToInt(payload, 2));
 		
 		return data;
 	}
+	
+	private static int byteArrayToInt(byte[] b, int offset) {
+        int value = 0;
+        for (int i = 0; i < 2; i++) {
+            int shift = (2 - 1 - i) * 8;
+            value += (b[i + offset] & 0x000000FF) << shift;
+        }
+        return value;
+    }
 }
